030f72dea729d586f0252d438880be316699c99b
jpmacleure <macleure-@hotmail.com>
2026-03-30 20:40:04 -0300

    add appweb_calculadora

--- appweb_calculadora/index.js
+++ appweb_calculadora/index.js
@@ -1,21 +1,51 @@
-// Carrega framework express
+// carrega framework express
 const express = require('express');
 
-// Cria um app usando a função express()
+const calculadora = require('./calculadora');
+
+// Cria um app do tipo express()
 const app     = express();
 
-// Define porta da aplicação
+// Declara porta de execução da aplicação
 const PORT = 8080;
 
-app.get('/', function (req, res){
-    res.send('<h1>Olá, mundo!</h1> <p>Estou na web...</p>');
+// Cria um array para armazenar respostas ao usuário
+const LOG = [];
+
+// Define rota raiz "/" e resposta da aplicação
+app.get('/', (req, res)=>{
+    res.send('Olá, WEB!');
 });
 
-app.get('/bem-vindo', function (req, res){
-    res.send('<h1>Bem-vindo ao desenvolvimento WEB!</h1>');
+// URL fixa
+app.get('/ola/joao', (req, res)=>{
+    res.send('Olá, João!');
 });
 
+// URL com parâmetro
+app.get('/ola/:nome', (req, res)=>{
+    res.send(`Olá, ${req.params.nome}!`);
+});
 
+// URL com múltiplos parâmetros
+app.get('/calculadora/somar/:a/:b', (req, res)=>{
+    let a = Number(req.params.a);
+    let b = Number(req.params.b);
+    let resultado = calculadora.somar(a, b);
+    let string_resultado = `<h1>${a} + ${b} = ${resultado}</h1>`;
+    LOG.push(string_resultado)
+    res.send(string_resultado);
+});
+
+app.get('/calculadora/log', (req, res)=>{
+    resultado = "";
+    LOG.forEach(log => {
+        resultado += log;
+    });
+    res.send(resultado);
+});
+
+// Inicializar a aplicação na porta definida
 app.listen(PORT, ()=>{
-    console.log('app rodando na porta ' + PORT);
+    console.log("app rodando na porta " + PORT);
 });
