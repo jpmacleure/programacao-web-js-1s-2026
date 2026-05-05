803861323903968ca9bef6b9fb4250b38499c29b
jpmacleure <macleure-@hotmail.com>
2026-05-04 21:14:27 -0300

    add validacao

--- app_agendamento_consulta/index.js
+++ app_agendamento_consulta/index.js
@@ -9,13 +9,86 @@ app.set('views', __dirname + '/views');
 app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res)=>{
-    res.render("index.html");
+
+    let dados_erros = {
+        erro_nome: req.query.erro_nome,
+        erro_sobrenome: req.query.erro_sobrenome,
+        erro_cpf: req.query.erro_cpf,
+        erro_data_nascimento: req.query.erro_data_nascimento
+    }
+    
+    let dados_form = {
+        nome: req.query.nome,
+        sobrenome: req.query.sobrenome,
+        cpf: req.query.cpf,
+        data_nascimento: req.query.data_nascimento
+    }
+    
+    res.render("index.html", {dados_erros, dados_form});
 });
 
 app.post('/agendamento', (req, res)=>{
-    dados_agendamento = req.body;
+    let dados_agendamento = req.body;
     console.log(dados_agendamento);
-    res.render('agendamento.html', {dados_agendamento});
+
+    let query_erros = ""
+    let existe_erro = false;
+
+    if(
+        dados_agendamento.nome == null ||
+        dados_agendamento.nome.length == 0 || 
+        dados_agendamento.nome.trim() == ""
+    ){
+        query_erros += "erro_nome=true&";
+        existe_erro = true;
+    }
+    else{
+        query_erros += `nome=${dados_agendamento.nome}&`;
+    }
+
+    if(
+        dados_agendamento.sobrenome == null ||
+        dados_agendamento.sobrenome.length == 0 || 
+        dados_agendamento.sobrenome.trim() == ""
+    ){
+        query_erros += "erro_sobrenome=true&";
+        existe_erro = true;
+    }
+    else{
+        query_erros += `sobrenome=${dados_agendamento.sobrenome}&`;
+    }
+
+    if(
+        dados_agendamento.cpf == null ||
+        dados_agendamento.cpf.length == 0 || 
+        dados_agendamento.cpf.trim() == ""
+    ){
+        query_erros += "erro_cpf=true&";
+        existe_erro = true;
+    }
+    else{
+        query_erros += `cpf=${dados_agendamento.cpf}&`;
+    }
+
+    if(
+        dados_agendamento.data_nascimento == null ||
+        dados_agendamento.data_nascimento.length == 0 || 
+        dados_agendamento.data_nascimento.trim() == ""
+    ){
+        query_erros += "erro_data_nascimento=true&";
+        existe_erro = true;
+    }
+    else{
+        query_erros += `data_nascimento=${dados_agendamento.data_nascimento}&`;
+    }
+
+    if(existe_erro){
+        res.redirect('/?'+query_erros);
+    }
+    else{
+        res.render('agendamento.html', {dados_agendamento});
+    }
+    
 });
 
 
